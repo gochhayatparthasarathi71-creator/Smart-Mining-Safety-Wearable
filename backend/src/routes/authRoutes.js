1f01535a3ed7c67438e2f6a138db526a2de9565f
gochhayatparthasarathi71-creator <gochhayatparthasarathi71@gmail.com>
2026-07-23 12:17:38 +0530

    Added auth test route

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const router = express.Router();
+
+// TEST ROUTE
+router.get('/test', (req, res) => {
+  res.json({
+    success: true,
+    message: "Auth route is working!"
+  });
+});
+
 const { login, me } = require('../controllers/authController');
 const { protect } = require('../middleware/authMiddleware');
 
